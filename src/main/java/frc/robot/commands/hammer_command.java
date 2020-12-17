9aeea93ee521ff99648507c0b7bc570a90961047
PM <polyversialmind@gmail.com>
2020-12-16 20:32:30 -0600

    completely redid hammer stuff went back to "old" class, the new "easier" class is not so easy

--- src/main/java/frc/robot/commands/hammer_command.java
+++ src/main/java/frc/robot/commands/hammer_command.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,41 +7,52 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class hammer_command extends CommandBase {
-  /**
-   * Creates a new hammer_command.
-   */
+public class hammer_command extends Command {
+  private boolean astat;
   public hammer_command() {
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(Robot.hammer_subsystem);
+    // Use requires() here to declare subsystem dependencies
+    // eg. requires(chassis);
+    requires(Robot.hammer_subsystem);
   }
 
-  // Called when the command is initially scheduled.
+  // Called just before this Command runs the first time
   @Override
-  public void initialize() {
+  protected void initialize() {
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  // Called repeatedly when this Command is scheduled to run
   @Override
-  public void execute() {
+  protected void execute() {
+    astat = Robot.m_oi.buttonA.get();
+    SmartDashboard.putBoolean("Hammer Button", astat);
+    //System.out.println("A stat:" + astat);
     if (Robot.m_oi.buttonA.get()) {
-      Robot.hammer_subsystem.hammerActivate();// other idea would be to make it take a boolean that instructs it whether to be active
+      Robot.hammer_subsystem.hammerActivate(0.3);// other idea would be to make it take a boolean that instructs it whether to be active
     } else {
+      Robot.hammer_subsystem.hammerActivate(0);// other idea would be to make it take a boolean that instructs it whether to be active
       //Robot.hammer_subsystem.hammerActivate(false);
     }
   }
 
-  // Called once the command ends or is interrupted.
+  // Make this return true when this Command no longer needs to run execute()
   @Override
-  public void end(boolean interrupted) {
+  protected boolean isFinished() {
+    return false;
   }
 
-  // Returns true when the command should end.
+  // Called once after isFinished returns true
   @Override
-  public boolean isFinished() {
-    return false;
+  protected void end() {
+    Robot.hammer_subsystem.hammerActivate(0.3);
+  }
+
+  // Called when another command which requires one or more of the same
+  // subsystems is scheduled to run
+  @Override
+  protected void interrupted() {
   }
 }
--- src/main/java/frc/robot/subsystems/hammer_subsystem.java
+++ src/main/java/frc/robot/subsystems/hammer_subsystem.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,27 +8,33 @@
 package frc.robot.subsystems;
 
 //import edu.wpi.first.wpilibj2.command.CommandScheduler;//https://docs.wpilib.org/en/stable/docs/software/commandbased/subsystems.html#setting-default-commands
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.commands.hammer_command;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.RobotMap;
+import frc.robot.commands.hammer_command;
 
-public class hammer_subsystem extends SubsystemBase {
+/**
+ * Add your docs here.
+ */
+public class hammer_subsystem extends Subsystem {
   WPI_TalonSRX hamMotor = null;
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
   /**
    * Creates a new hammer_subsystem.
    */
   public hammer_subsystem() {
-    setDefaultCommand(new hammer_command());
-    hamMotor = new WPI_TalonSRX(RobotMap.BUTTON_A);
+    hamMotor = new WPI_TalonSRX(RobotMap.MOTOR_HAMMER_1);
   }
 
   @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new hammer_command());
   }
 
-  public void hammerActivate() {
-    hamMotor.set(0.3);
+  public void hammerActivate(double spd) {
+    hamMotor.set(spd);
   }
 }
